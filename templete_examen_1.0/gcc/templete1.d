6767500e54bb954ca80cbbd8c27846431d59cae0
rod-0310 <frodriguezcopa@gmail.com>
2024-09-21 01:24:20 -0400

    en esta carpeta esta timer,uart,pwm,adc,gpios en uno solo

---
+++ templete_examen_1.0/gcc/templete1.d
@@ -0,0 +1,13 @@
+gcc/templete1.o: templete1.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../../../../inc/hw_memmap.h ../../../../driverlib/sysctl.h \
+ ../../../../driverlib/gpio.h ../../../../driverlib/pin_map.h \
+ ../../../../driverlib/timer.h ../../../../driverlib/interrupt.h \
+ ../../../../inc/hw_ints.h ../../../../driverlib/pwm.h \
+ ../../../../driverlib/uart.h ../../../../utils/uartstdio.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ ../../../../inc/hw_types.h ../../../../inc/hw_uart.h \
+ ../../../../driverlib/debug.h ../../../../driverlib/rom.h \
+ ../../../../driverlib/rom_map.h ../../../../utils/uartstdio.h \
+ ../../../../driverlib/adc.h
