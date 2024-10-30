01c90282a20bc4ce1d1e144b9ab523de86cce64a
rod-0310 <frodriguezcopa@gmail.com>
2024-10-29 20:49:05 -0400

    sss

--- miniproy/raspy/pwm_urt_proyecto.py
+++ miniproy/raspy/pwm_urt_proyecto.py
@@ -5,29 +5,14 @@ from time import sleep
 from gpiozero import PWMLED
 
 # Configuración del puerto serial
-tiva = serial.Serial("/dev/ttyACM0", 115200)
+tiva = serial.Serial("/dev/ttyACM0", 9600)
 tiva.reset_input_buffer()
-
-# Configuración de los pines de control de dirección de los motores
-motor1der = 4
-motor1izq = 5
-motor2der = 6
-motor2izq = 7
-
 # Configuración de PWM en los pines GPIO 13 y GPIO 19 para controlar la velocidad
 vel1 = PWMLED(13)
 vel2 = PWMLED(19)
 
 # Configuración de los pines GPIO para controlar la dirección del motor
 chip = gpiod.Chip('gpiochip0')
-der1 = chip.get_line(motor1der)
-der2 = chip.get_line(motor2der)
-izq1 = chip.get_line(motor1izq)
-izq2 = chip.get_line(motor2izq)
-der1.request(consumer="derecha M1", type=gpiod.LINE_REQ_DIR_OUT)
-der2.request(consumer="derecha M2", type=gpiod.LINE_REQ_DIR_OUT)
-izq1.request(consumer="izquierda M1", type=gpiod.LINE_REQ_DIR_OUT)
-izq2.request(consumer="izquierda M2", type=gpiod.LINE_REQ_DIR_OUT)
 
 # Función para procesar y actualizar los PWM en función del valor ADC recibido
 def opcion_boton(valor):
@@ -36,30 +21,18 @@ def opcion_boton(valor):
             # Detener ambos motores
             vel1.value = 0  # Apagar Motor 1
             vel2.value = 0  # Apagar Motor 2
-            der1.set_value(0)
-            der2.set_value(0)
-            izq1.set_value(0)
-            izq2.set_value(0)
             print("Motores detenidos")
 
         elif valor == "amarillo":
             # Configurar ambos motores al 35% de velocidad
-            vel1.value = 0.35  # Motor 1 al 35%
-            vel2.value = 0.35  # Motor 2 al 35%
-            der1.set_value(1)
-            der2.set_value(1)
-            izq1.set_value(0)
-            izq2.set_value(0)
+            vel1.value = 0.25  # Motor 1 al 35%
+            vel2.value = 0.25  # Motor 2 al 35%
             print("Motores al 35% de velocidad")
 
         elif valor == "verde":
             # Configurar ambos motores al 80% de velocidad
-            vel1.value = 0.8  # Motor 1 al 80%
-            vel2.value = 0.8  # Motor 2 al 80%
-            der1.set_value(1)
-            der2.set_value(1)
-            izq1.set_value(0)
-            izq2.set_value(0)
+            vel1.value = 1 # Motor 1 al 80%
+            vel2.value = 1  # Motor 2 al 80%
             print("Motores al 80% de velocidad")
             
     except ValueError:
