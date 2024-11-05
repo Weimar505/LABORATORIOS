9dfcbdedd3ad8287f5e08722130a44798a5a77f6
rod-0310 <frodriguezcopa@gmail.com>
2024-11-05 10:18:16 -0400

    ejemplo pwmm en tiva

---
+++ pwm/gcc/ejemplo1.d
@@ -0,0 +1,6 @@
+gcc/ejemplo1.o: ejemplo1.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ ../../../../inc/hw_memmap.h ../../../../driverlib/gpio.h \
+ ../../../../driverlib/pin_map.h ../../../../driverlib/pwm.h \
+ ../../../../driverlib/sysctl.h
